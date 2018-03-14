9aaddf3676bc5e4e39d46d2a0c820d87e9883dd4
Kritika <rkritika1508@gmail.com>
2018-03-14 22:45:21 +0530

    Added simple timer

--- clock.js
+++ clock.js
@@ -9,6 +9,7 @@ var minBreakDisplay = document.querySelector(".breakDisplay");
 var stopBtn = document.querySelector("#stop");
 var startBtn = document.querySelector("#start");
 var resetBtn = document.querySelector("#reset");
+var timer = document.getElementById("timeDisplay");
 function minSesDisplay(){
 	minSessionDisplay.textContent = minSession;
 }
@@ -38,4 +39,23 @@ resetBtn.addEventListener("click", function(){
 	minBreak = 5;
 	minSesDisplay();
 	minBrDisplay();
+})
+timer.textContent = minSession + ":" + 00;
+function startTimer(){
+	var presentTime = timer.textContent;
+	var timeArray = presentTime.split(/[:]+/);
+	var m = timeArray[0];
+ 	var s = checkSecond((timeArray[1] - 1));
+	if(s==59){m=m-1}
+  	if(m<0){alert('timer completed')}
+	timer.textContent = m + ":" + s;
+	setTimeout(startTimer, 1000);
+}
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+startBtn.addEventListener("click", function(){
+	startTimer();
 })
